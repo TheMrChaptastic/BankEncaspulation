e4cfc14ad4d556fda59cb76cdc9dbd35f0d7f713
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-20 10:55:31 -0600

    Finished

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Deposit(double ammount)
+        {
+            balance = ammount;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAcc = new BankAccount();
+            Console.WriteLine(myAcc.Balance);
+            myAcc.Deposit(120.01);
+            Console.WriteLine(myAcc.Balance);
+        }
+    }
+}
